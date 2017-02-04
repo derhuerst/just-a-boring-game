0eecdc305ea3d1c6a5007dc374ee3b17ce675304
Leon Ochmann <leonochmann@outlook.com>
2017-02-04 01:14:50 +0100

    Adds the infamous WebGL 7 cube rendering UI.

---
+++ birds-eye-camera.js
@@ -0,0 +1,122 @@
+'use strict'
+
+const vec2 = require('gl-matrix').vec2
+const vec3 = require('gl-matrix').vec3
+const mat2 = require('gl-matrix').mat2
+const mat3 = require('gl-matrix').mat3
+const mat4 = require('gl-matrix').mat4
+
+
+
+// prototype of all birds eye cameras
+const birdsEyeProto = {
+	// wsad: An array of 4 ints, usually the number of times w, s, a and d have been pressed since the last call, respectively.
+	move: function (wsad) {
+		let change = vec2.create()
+		change[0] += (wsad[0] - wsad[1]) / 50
+		change[1] += (wsad[2] - wsad[3]) / 50 // The signs are inverted here because the x-axis points to the right, but a goes to the left.
+
+		let rotation = mat2.create()
+		mat2.rotate(rotation, rotation, this.rotationY)
+		vec2.transformMat2(change, change, rotation)
+
+		let distanceCovered = this.speed * this.distance / 10
+		vec2.scale(change, change, distanceCovered)
+
+		this.target[0] -= change[1] // -x is left
+		this.target[2] -= change[0] // -z is forwards
+	},
+
+	// rotation: Usually the number of pixels scrolled left (positive) or right (negative) since the last call.
+	rotate: function (rotation) {
+		this.rotationY += Math.PI * rotation / 500 // todo: rotation speed
+	},
+
+	// zoom: Usually the number of pixels scrolled up (negative) or down (positive) since the last call.
+	zoom: function (zoom) {
+		this.distance *= Math.exp(zoom / 500) // todo: zoom speed
+	},
+
+	// this.preRotateWorld is determined to be the  4x4 matrix that transforms the world such that this.up is projected to the y axis and this.alignment to the z axis.
+	setPlane: function (up, alignment, thirdAxis) {
+
+		if (!thirdAxis) {
+			thirdAxis = vec3.create()
+			vec3.cross(thirdAxis, this.up, this.alignment)
+			vec3.normalize(thirdAxis, thirdAxis)
+		}
+
+		this.up = up
+		this.alignment = alignment
+		this.thirdAxis = thirdAxis
+
+		thirdAxis = [ thirdAxis[0], thirdAxis[1], thirdAxis[2] ] // make sure this is a dynamic array, so that .concat is available
+		let preRotateWorld = mat3.fromValues(...(thirdAxis.concat(this.up, this.alignment)))
+		mat3.invert(preRotateWorld, preRotateWorld)
+		preRotateWorld = mat4.fromValues(
+			preRotateWorld[0], preRotateWorld[1], preRotateWorld[2], 0,
+			preRotateWorld[3], preRotateWorld[4], preRotateWorld[5], 0,
+			preRotateWorld[6], preRotateWorld[7], preRotateWorld[8], 0,
+			0, 0, 0, 1
+		)
+
+		this.preRotateWorld = preRotateWorld
+	},
+
+	view: function () {
+		let view = mat4.create()
+
+		// This was never necessary:
+		// let cameraRotation = mat4.create()
+		// mat4.rotateX(cameraRotation, cameraRotation, this.rotationX)		
+		// mat4.rotateY(cameraRotation, cameraRotation, this.rotationY)
+		// let cameraRelativeToTarget = [ 0, 0, this.distance ]
+		// vec3.transformMat4(cameraRelativeToTarget, cameraRelativeToTarget, cameraRotation)
+		// let invCameraPosition = vec3.create()
+		// vec3.add(invCameraPosition, this.target, cameraRelativeToTarget)
+		// vec3.scale(invCameraPosition, invCameraPosition, -1)
+		// // console.log(invCameraPosition)
+
+		// The following is exactly what we want to do, trust me.
+		mat4.translate(view, view, [ 0, 0, -this.distance ])
+		mat4.rotateX(view, view, -this.rotationX)
+		mat4.rotateY(view, view, -this.rotationY)
+		mat4.translate(view, view, [ -this.target[0], -this.target[1], -this.target[2] ])
+		mat4.multiply(view, view, this.preRotateWorld)
+
+		return view
+	}
+}
+
+
+// members
+//     read-only
+//         .up
+//         .alignment
+//         .thirdAxis (use .setPlane(up, alignment, thirdAxis) for these 3)
+//     read-write
+//         .distance
+//         .rotationX
+//         .speed -- units per key press at the default distance 10. (The actual camera movement speed scales linearly with .distance.)
+//         to be changed directly or by .control(dt, wsad, rotate)
+//             .rotationY
+//              .target -- specifies where to look at in plane coordinates
+const createBirdsEye = (options = {}) => {
+	let b = Object.create(birdsEyeProto)
+
+	// default values:
+	b.up        = [ 0, 1, 0 ] // should be normalized
+	b.alignment = [ 0, 0, 1 ] // should be orthogonal to b.up and normalized
+	b.distance  = 10
+	b.rotationX = - Math.PI / 4
+	b.rotationY = 0 // todo: defineProperty read only
+	b.target    = [ 0, 0, 0 ]
+	b.speed     = 5 // units covered per second
+	Object.assign(b, options)	
+	
+	b.setPlane(b.up, b.alignment, b.thirdAxis)
+
+	return b
+}
+
+module.exports = createBirdsEye
--- ui.js
+++ ui.js
@@ -1,19 +1,173 @@
 'use strict'
 
-const {EventEmitter} = require('events')
 
-const el = document.createElement('input')
-document.body.appendChild(el)
 
-const change = (value) => {
-	el.value = value
-}
+const normals = require('normals')
+const mat4 = require('gl-matrix').mat4
+const vec3 = require('gl-matrix').vec3
+const vec4 = require('gl-matrix').vec4
 
-el.addEventListener('keypress', () => {
-	ui.emit('change', el.value)
+const shell = require('gl-now')({
+	tickRate: 10
 })
 
-const ui = new EventEmitter()
-ui.change = change
+shell.bind("move-left", "left", "A")
+shell.bind("move-right", "right", "D")
+shell.bind("move-forward", "up", "W")
+shell.bind("move-backward", "down", "S")
+shell.bind("rotate-left", "Q")
+shell.bind("rotate-right", "E")
 
-module.exports = ui
+
+
+const glslify = require('glslify')
+const createShader = require('gl-shader')
+
+
+const createGeometry = require('gl-geometry')
+
+const cube = require('primitive-cube')()
+
+const createCamera = require('./birds-eye-camera.js')
+
+
+
+
+
+const pick = require('camera-picking-ray')
+const Ray = require('ray-3d')
+ 
+
+
+
+
+
+let camera
+let cubeGeometry
+let gutterGeometry
+let shader
+
+shell.on("gl-init", function() {
+	shell.gl.enable(shell.gl.DEPTH_TEST)
+	//shell.gl.enable(shell.gl.CULL_FACE)
+
+	// camera
+	camera = createCamera({
+		alignment: [ 0.707, 0, 0.707 ],
+		target: [ 0, 0, 0 ],
+		distance: 10,
+		speed: 20
+	})
+
+	cubeGeometry = createGeometry(shell.gl)
+	cubeGeometry.attr('position', cube.positions)
+	cubeGeometry.attr('normal', cube.normals)
+	cubeGeometry.faces(cube.cells)
+
+	let gutterPositions = []
+	for (let i = -10; i <= 10; i++) {
+		gutterPositions.push([ i, 0, -10 ], [ i, 0, 10 ])
+		gutterPositions.push([ -10, 0, i ], [ 10, 0, i ])
+	}
+	gutterGeometry = createGeometry(shell.gl)
+	gutterGeometry.attr('position', gutterPositions)
+
+	// shader
+	shader = createShader(shell.gl,
+	    glslify('./shaders/bunny.vert')
+	  , glslify('./shaders/bunny.frag')
+	)
+})
+
+
+
+
+let projection  = mat4.create()
+let view        = mat4.create()
+let gutterModel = mat4.create()
+let cubeModels  = [ [], [], [], [], [], [], [] ]
+cubeModels[0] = mat4.create()
+mat4.translate(cubeModels[0], cubeModels[0], [ -7.5, .5, -7.5 ])
+for (let i = 1; i <= 6; i++)
+	mat4.translate(cubeModels[i], cubeModels[i-1], [ 2, 0, 2 ])
+let red = [ 1, 0, 0 ]
+let black = [ 0, 0, 0 ]
+
+let hit
+
+shell.on('tick', (() => {
+	let width = 20
+	return () => {
+		camera.move([
+			shell.down("move-forward"),
+			shell.down("move-backward"),
+			shell.down("move-left"),
+			shell.down("move-right")
+		])
+		camera.rotate(shell.scroll[0])
+		camera.zoom(shell.scroll[1]) // Kinda unnecessary given that we employ an orthographic projection later.
+		view = camera.view()
+
+		const fieldOfView = Math.PI / 4
+		let aspectRatio = shell.width / shell.height
+		const near = 0.01 	
+		const far  = 500
+		width *= Math.exp(shell.scroll[1]/500) // zooming
+		let height = width / aspectRatio
+
+		mat4.ortho(
+			projection,
+			-width/2, width/2, -height/2, height/2,
+			near, far)
+
+		let invProjView = mat4.create()
+		mat4.multiply(invProjView, projection, view)
+		mat4.invert(invProjView, invProjView)
+		hit = cubeModels.findIndex((cubeModel) => {
+			let invModel = mat4.create()
+			let invProjViewModel = mat4.create()
+			mat4.invert(invModel, cubeModel)
+			mat4.multiply(invProjViewModel, invModel, invProjView)
+
+			let r = {
+				origin: vec3.create(),
+				direction: vec3.create()
+			}
+			pick(r.origin, r.direction, [ shell.mouse[0], shell.height - shell.mouse[0] ], [ 0, 0, shell.width, shell.height ], invProjViewModel)
+			let ray = new Ray(r.origin, r.direction)
+
+			return cube.cells.some((cell, i) => !!ray.intersectsTriangleCell(cell, cube.positions))
+		})
+	}})())
+
+
+
+
+shell.on("gl-render", function(dt) {
+	shader.uniforms.uProjection = projection
+	shader.uniforms.uView = view
+	
+	cubeGeometry.bind(shader)
+	for (let cubeModel of cubeModels) {
+		shader.uniforms.uModel = cubeModel
+		shader.uniforms.uColor = (hit === cubeModel) ? red : black
+		cubeGeometry.draw(shell.gl.TRIANGLES)
+	}
+	
+	shader.uniforms.uModel = gutterModel
+	shader.uniforms.uColor = black
+	gutterGeometry.bind(shader)
+	gutterGeometry.draw(shell.gl.LINES)
+})
+
+shell.on("gl-error", function(e) {
+	throw new Error("WebGL not supported :(")
+})
+
+module.exports = {
+	selectCube: (i) => {
+		hit = i
+	},
+
+	onCubeSelect: () => {}
+}
